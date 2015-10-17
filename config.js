a0f2abb5d739569ea75f6008f525182378e747a8
Anthonyeef <anthonyeef@gmail.com>
2015-10-18 01:22:26 +0800

    test

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  author: {
+    name: "Joyee Cheung",
+    url: "https://github.com/joyeecheung"
+  },
+  repo: {  // for the markdown source
+    content: "https://github.com/joyeecheung/my-tech-diary",
+    as_submodule: 'diary'
+  },
+  base_url: '/diary',  // remote directory for the website
+  ga_id: 'UA-59628523-1' // optional: Google Analysis ID 
+  title: 'My Tech Diary'
+}
